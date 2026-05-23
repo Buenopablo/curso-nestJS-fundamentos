2d6939820f1fa2476cf9da04c98b05996817e7d4
Pablo <pablobuenoctn18@gmail.com>
2026-05-23 13:02:59 -0300

    feat: add prisma config and module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,19 @@
-// import { Injectable } from '@nestjs/common';
-// import { CreateUserDTO } from './dto/create-user.dto';
-// import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-// @Injectable()
-// export class UserService {
-//   constructor(private readonly prisma: PrismaService) {}
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
 
-//   async create({ email, name, password }: CreateUserDTO) {
-//     const user = await this.prisma.users.create({
-//       data: {
-//         email,
-//         name,
-//         password,
-//       },
-//     });
-//     return user;
-//   }
-// }
+  async create({ email, name, password }: CreateUserDTO) {
+    const user = await this.prisma.users.create({
+      data: {
+        email,
+        name,
+        password,
+      },
+    });
+    return user;
+  }
+}
