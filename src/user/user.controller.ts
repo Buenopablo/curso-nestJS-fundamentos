1cc90fe3eec25d615cb4677f71e489e0c2ab7e10
Pablo <pablobuenoctn18@gmail.com>
2026-05-22 21:51:24 -0300

    Initial commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body: CreateUserDTO) {
+    // console.log(body);
+    return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get('/:id')
+  async readOne(@Param('id', ParseIntPipe) id) {
+    return { user: {}, id };
+  }
+
+  @Put('/:id')
+  async update(@Body() body: UpdatePutUserDTO, @Param('id', ParseIntPipe) id) {
+    return {
+      method: 'put',
+      body,
+      id,
+    };
+  }
+
+  @Patch('/:id')
+  async updatePartial(
+    @Body() body: UpdatePatchUserDTO,
+    @Param('id', ParseIntPipe) id,
+  ) {
+    return {
+      method: 'patch',
+      body,
+      id,
+    };
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', ParseIntPipe) id) {
+    return { id };
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+// import { Injectable } from '@nestjs/common';
+// import { CreateUserDTO } from './dto/create-user.dto';
+// import { PrismaService } from 'src/prisma/prisma.service';
+
+// @Injectable()
+// export class UserService {
+//   constructor(private readonly prisma: PrismaService) {}
+
+//   async create({ email, name, password }: CreateUserDTO) {
+//     const user = await this.prisma.users.create({
+//       data: {
+//         email,
+//         name,
+//         password,
+//       },
+//     });
+//     return user;
+//   }
+// }
