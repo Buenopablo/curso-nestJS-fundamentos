e037e89f75a4df24a06e757aa3d50e97652b8cc9
Pablo Bueno <pablo.bueno@wihex.com.br>
2026-06-09 13:56:01 -0300

    feat: add multi uploads

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -14,9 +15,14 @@ import { AuthResetDTO } from './dto/auth-reset.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
 import { join } from 'path';
 import { FileService } from 'src/file/file.service';
+import { Express } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -71,5 +77,33 @@ export class AuthController {
     }
 
     return { sucess: true };
+  }
+
+  @UseInterceptors(FilesInterceptor('files'))
+  @UseGuards(AuthGuard)
+  @Post('files')
+  async uploadFiles(@User() user, @UploadedFiles() files: []) {
+    return files;
+  }
+
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      {
+        name: 'photo',
+        maxCount: 1,
+      },
+      {
+        name: 'documents',
+        maxCount: 10,
+      },
+    ]),
+  )
+  @UseGuards(AuthGuard)
+  @Post('files-fields')
+  async uploadFilesFields(
+    @User() user,
+    @UploadedFiles() files: { photo; File; documents: [] },
+  ) {
+    return { files };
   }
 }
