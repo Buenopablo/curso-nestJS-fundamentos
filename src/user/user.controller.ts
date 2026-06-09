72ffb9d1bf32ccd96d57f472f761c00a99d581e5
Pablo Bueno <pablo.bueno@wihex.com.br>
2026-06-09 09:17:43 -0300

    feat: autorization RBAC party 2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,44 +20,39 @@ import { Role } from 'src/enums/role.enum';
 import { RoleGuard } from 'src/guards/role.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 
+@Roles(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Roles(Role.Admin)
   @UseInterceptors(LogInterceptor)
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data);
   }
 
-  @Roles(Role.Admin)
   @Get()
   async list() {
     return this.userService.listAll();
   }
 
-  @Roles(Role.Admin)
   @Get('/:id')
   async readOne(@ParamId() id: number) {
     console.log({ id });
     return this.userService.getById(id);
   }
 
-  @Roles(Role.Admin)
   @Put('/:id')
   async update(@Body() data: UpdatePutUserDTO, @ParamId() id) {
     return this.userService.update(id, data);
   }
 
-  @Roles(Role.Admin)
   @Patch('/:id')
   async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id) {
     return this.userService.updatePartial(id, data);
   }
 
-  @Roles(Role.Admin)
   @Delete('/:id')
   async delete(@ParamId() id) {
     return this.userService.delete(id);
