75ed09af0abb388bb6ecb245747bd2f97be1c5dd
Pablo Bueno <pablo.bueno@wihex.com.br>
2026-06-10 08:31:46 -0300

    feat: add typeorm

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,6 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { users } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthRegisterDTO } from './dto/auth-register.dto ';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
@@ -15,7 +13,6 @@ import { MailerService } from '@nestjs-modules/mailer/dist';
 export class AuthService {
   constructor(
     private readonly JWTService: JwtService,
-    private readonly prisma: PrismaService,
     private readonly userService: UserService,
     private readonly mailer: MailerService,
   ) {}
